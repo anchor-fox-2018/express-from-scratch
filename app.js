5a353781eff1b4618af3673f91c25d221722b366
anastasia <anastasia.santoso.id@gmail.com>
2018-09-23 19:58:38 +0700

    Release 4

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+app.set('view engine', 'ejs')
 const router = require('./routes')
 const port = 3000
 
 app.use('/', router)
+app.use('/student', router)
+app.use('/teacher', router)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Check localhost ${port}!`))
