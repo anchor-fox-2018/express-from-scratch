6fcb67425f2acfe0d325b29cbaefb0e5a07c0533
anastasia <anastasia.santoso.id@gmail.com>
2018-09-23 18:11:46 +0700

    Release 2

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get('/', (req, res) => res.send('I love Hactiv8'))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
